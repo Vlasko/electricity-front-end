fc68ff28b1f06825fdbd8daef23d8656e3f64b75
Vlasko <luka.j.v@gmail.com>
2020-08-27 16:20:42 +0100

    remove Topic test model

--- octopus_display/smart_plug/admin.py
+++ octopus_display/smart_plug/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from smart_plug.models import Topic, Instance
+from smart_plug.models import Instance
 # Register your models here.
-admin.site.register(Topic)
+
 admin.site.register(Instance)
