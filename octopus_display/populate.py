6a666e51dc08bdcab4a0ea2d41f8c9884e3459f0
Vlasko <luka.j.v@gmail.com>
2020-08-27 11:12:37 +0100

    lay foundations for populate

--- octopus_display/populate.py
+++ octopus_display/populate.py
@@ -8,11 +8,20 @@ from smart_plug.models import Instance
 
 # Third Party Imports
 import pandas as pd
-import glob
 
 # Pull csv data into script
 file  = '/Users/gianluca/Documents/Projects/electricity-processing/files/cleaned_data/2019-12-18_2020-07-19.csv'
 data = pd.read_csv(file,index_col='Timestamp')
 
 def populate(data):
-    #figure out how to populate database here
+    for timestamp, row in data.iterrows():
+        demand = row[0]
+        unit_price = row[1]
+        price = row[2]
+
+        inst = Instance.objects.get_or_create(timestamp = timestamp,
+                                              demand = demand,
+                                              unit_price = unit_price,
+                                              price = price
+                                              )
+    
