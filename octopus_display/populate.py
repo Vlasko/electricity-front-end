949b339666bbaffe7edf8559675ae005036b6bab
Vlasko <luka.j.v@gmail.com>
2020-08-27 10:21:55 +0100

    simplify script as data has been cleaned in original repo

--- octopus_display/populate.py
+++ octopus_display/populate.py
@@ -4,32 +4,15 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'octopus_display.settings')
 import django
 django.setup()
 
+from smart_plug.models import Instance
+
+# Third Party Imports
 import pandas as pd
 import glob
-from smart_plug.models import Demand
 
-# Import csv data from
-
-def data_pull():
-    directory  = '/Users/gianluca/Documents/Projects/electricity-processing/files/'
-    datatype = ['demand/', 'prices/']
-    filetype = '*.csv'
-
-    type_dict = {}
-    for type in datatype:
-        data_list=[]
-        for file in glob.glob(directory + type + filetype):
-            data = pd.read_csv(os.path.join(directory, file),
-                               index_col='Timestamp')
-            data_list.append(data)
-        data_combined = pd.concat(data_list)
-        type_dict.update({type[:-1]:data_combined})
-
-    return type_dict
-
-imported_data_dict = data_pull()
-
-imported_data_dict['demand']
+# Pull csv data into script
+file  = '/Users/gianluca/Documents/Projects/electricity-processing/files/cleaned_data/2019-12-18_2020-07-19.csv'
+data = pd.read_csv(file,index_col='Timestamp')
 
 def populate(data):
     #figure out how to populate database here
