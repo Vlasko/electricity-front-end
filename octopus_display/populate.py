d6c3da25c9e9093f5daa2fea7f5e7ecb5daf81f1
Vlasko <luka.j.v@gmail.com>
2020-08-27 16:20:29 +0100

    updated populate script to bring data from csv to db

--- octopus_display/populate.py
+++ octopus_display/populate.py
@@ -4,24 +4,28 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'octopus_display.settings')
 import django
 django.setup()
 
+#Import models
 from smart_plug.models import Instance
 
 # Third Party Imports
 import pandas as pd
 
 # Pull csv data into script
-file  = '/Users/gianluca/Documents/Projects/electricity-processing/files/cleaned_data/2019-12-18_2020-07-19.csv'
-data = pd.read_csv(file,index_col='Timestamp')
+def populate():
+    file  = '/Users/gianluca/Documents/Projects/electricity-processing/files/cleaned_data/2019-12-18_2020-07-19.csv'
+    collected_data = pd.read_csv(file,index_col='Timestamp')
 
-def populate(data):
-    for timestamp, row in data.iterrows():
-        demand = row[0]
-        unit_price = row[1]
-        price = row[2]
+    for timestamp, row in collected_data.iterrows():
+        demand = round(row[0],2)
+        unit_price = round(row[1],2)
+        price = round(row[2],2)
 
         inst = Instance.objects.get_or_create(timestamp = timestamp,
-                                              demand = demand,
-                                              unit_price = unit_price,
-                                              price = price
-                                              )
-    
+                demand = demand,
+                unit_price = unit_price,
+                price = price)[0]
+
+if __name__ == '__main__':
+    print('populating script!')
+    populate()
+    print('populating complete!')
